48fc19aed5a6e9c518a8aa0c93d700347b3b2892
memset0 <34177126+memset0@users.noreply.github.com>
2021-01-13 15:57:51 +0800

    文哥测号器

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,8 @@
+copy 测号器\index.html static\test.html
+copy 测号器\index.html static\test\index.html
+
+git pull
+git add .
+git commit -m "upd"
+git push
 ssh aliyun-server "cd /www/git/namerena-utils && git pull"
